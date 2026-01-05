76b73e27085bbd02e862cac2944a8977edf88527
bal7hazar <bal7hazar@proton.me>
2026-01-05 13:40:09 +0100

    chore: update storybook packages

--- client/src/components/containers/game-scene.stories.tsx
+++ client/src/components/containers/game-scene.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { fn } from "@storybook/test";
+import { fn } from "storybook/test";
 import { GameScene } from "./game-scene";
 
 const meta: Meta<typeof GameScene> = {
--- client/src/components/elements/puller.stories.tsx
+++ client/src/components/elements/puller.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { fn } from "@storybook/test";
+import { fn } from "storybook/test";
 import { Puller } from "./puller";
 
 const meta = {
