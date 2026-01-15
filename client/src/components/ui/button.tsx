c9289c8ee877a1b094db4218f52d94a8d96e0d48
Nasr <larko236@outlook.com>
2026-01-15 12:00:52 -0600

    style: make default button same as secondary (bg-green-950)

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-green-900 hover:bg-green-800 text-primary rounded-lg",
+        default: "bg-green-950 hover:bg-green-900 text-primary rounded-lg",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
