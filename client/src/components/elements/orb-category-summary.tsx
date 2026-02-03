22f45457e90efd943bfc5f9dc33f3e5cdc103912
Nasr <larko236@outlook.com>
2026-02-03 14:33:25 -0600

    Tweak summary pill border and glow

--- client/src/components/elements/orb-category-summary.tsx
+++ client/src/components/elements/orb-category-summary.tsx
@@ -144,14 +144,14 @@ export const OrbCategorySummary = ({
                   orb={config.orb}
                   size="xs"
                   showValue={false}
-                  glowScale={0.35}
+                  glowScale={0.7}
                 />
                 {/* Count pill */}
                 <div
                   className="absolute -bottom-1.5 left-1/2 -translate-x-1/2 px-1.5 py-[1px] rounded-full text-[9px] font-bold font-secondary"
                   style={{
                     backgroundColor: darkenHex(config.color, 0.35),
-                    border: `1.5px solid ${config.color}`,
+                    border: `1px solid ${config.color}`,
                     color: config.color,
                   }}
                 >
