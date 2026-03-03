13d174af45ffcb95fe8fb790ae69df3b20bba8a9
Nasr <larko236@outlook.com>
2026-03-03 13:07:12 -0600

    Change font-secondary to Rubik One

--- client/src/themes/preset.ts
+++ client/src/themes/preset.ts
@@ -112,7 +112,7 @@ export const preset: Partial<Config> = {
       fontFamily: {
         body: ["Rubik One", "sans"],
         glitch: ["Rubik Glitch", "sans"],
-        secondary: ["VCR OSD Mono", "sans"],
+        secondary: ["Rubik One", "sans"],
         primary: ["Pixel Game", "Rubik One", "sans"],
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
