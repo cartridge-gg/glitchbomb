e8cd78168c0aeee8a209d3cbf9c4528b120773d3
Larkooo <larko236@outlook.com>
2026-03-10 14:52:26 -0500

    Remove unused useRef import

--- client/src/hooks/use-audio.ts
+++ client/src/hooks/use-audio.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import type { Orb } from "@/models/orb";
 import { OrbType } from "@/models/orb";
 
